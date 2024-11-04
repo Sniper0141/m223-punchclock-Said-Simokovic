37146d00e55ad1e28303ae009270af73daa19fd5
Said Simokovic <116571645+Sniper0141@users.noreply.github.com>
2024-11-04 15:15:16 +0100

    small cleanup in service

--- Service/EntryService.cs
+++ Service/EntryService.cs
@@ -3,22 +3,17 @@ using Microsoft.EntityFrameworkCore;
 
 namespace M223PunchclockDotnet.Service
 {
-    public class EntryService
+    public class EntryService(DatabaseContext databaseContext)
     {
-        private DatabaseContext _databaseContext; 
-
-        public EntryService(DatabaseContext databaseContext){
-            _databaseContext = databaseContext;
-        }
         public Task<List<Entry>> FindAll()
         {
-            return _databaseContext.Entries.ToListAsync();
+            return databaseContext.Entries.ToListAsync();
         }
 
         public async Task<Entry> AddEntry(Entry entry)
         {
-            _databaseContext.Entries.Add(entry);
-            await _databaseContext.SaveChangesAsync();
+            databaseContext.Entries.Add(entry);
+            await databaseContext.SaveChangesAsync();
 
             return entry;
         }
