b09b047d50460f0437ce15f2cc0a3ca475f64205
Said Simokovic <116571645+Sniper0141@users.noreply.github.com>
2024-11-13 10:52:36 +0100

    add keys

--- Program.cs
+++ Program.cs
@@ -2,14 +2,18 @@ using System.Text.Json.Serialization;
 using Microsoft.EntityFrameworkCore;
 using M223PunchclockDotnet.Service;
 using M223PunchclockDotnet.Model;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Cryptography;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
+// MVC
 builder.Services.AddControllers().AddJsonOptions(x =>
     x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve);
-    
+
+// Databse
 builder.Services.AddDbContext<DatabaseContext>(options =>
     options.UseNpgsql("Host=db;Database=postgres;Username=postgres;Password=postgres"));
 
@@ -17,11 +21,33 @@ builder.Services.AddDbContext<DatabaseContext>(options =>
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
+// Services & Repositories
 builder.Services.AddScoped<EntryService, EntryService>();
 builder.Services.AddScoped<CategoryService, CategoryService>();
 builder.Services.AddScoped<TagService, TagService>();
 builder.Services.AddScoped<TestDataRepository, TestDataRepository>();
+
+// JWT-Authentifizierung
+var publicKey = RSA.Create();
+publicKey.ImportFromPem(System.IO.File.ReadAllText("Keys/public.key"));
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new RsaSecurityKey(publicKey) // Verifizierung mit öffentlichem Schlüssel
+        };
+    });
+builder.Services.AddAuthorization();
+
 
 var app = builder.Build();
 
@@ -51,6 +77,7 @@ using (var scope = app.Services.CreateScope())
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
